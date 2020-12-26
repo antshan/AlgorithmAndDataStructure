99abe9326e626e51f50e58867408ea4146726a23
wei shan <weishan@weideMacBook-Pro.local>
2020-12-26 13:20:49 +0800

    feat:find first intersection node in list

--- src/main/java/com/basic/algorithm/class06/Code05_FindFirstIntersectNode.java
+++ src/main/java/com/basic/algorithm/class06/Code05_FindFirstIntersectNode.java
@@ -1,18 +1,13 @@
 package com.basic.algorithm.class06;
 
+import com.basic.algorithm.Node;
+
+import java.util.Map;
+
 /**
  * 两个单链表可能有环或者无环相交，求他们相交的节点
  */
 public class Code05_FindFirstIntersectNode {
-
-    public static class Node {
-        public int value;
-        public Node next;
-
-        public Node(int value) {
-            this.value = value;
-        }
-    }
 
     public static Node getIntersectNode(Node head1, Node head2) {
         if (head1 == null || head2 == null) {
@@ -22,8 +17,7 @@ public class Code05_FindFirstIntersectNode {
         Node loop2 = getLoopNode(head2);
         if (loop1 == null && loop2 == null) {
             return noLoop(head1, head2);
-        }
-        if (loop1 != null && loop2 != null) {
+        } else if (loop1 != null && loop2 != null) {
             return bothLoop(head1, loop1, head2, loop2);
         }
         return null;
@@ -36,7 +30,6 @@ public class Code05_FindFirstIntersectNode {
         }
         Node n1 = head.next;
         Node n2 = head.next.next;
-        // 如果有环 一定能找到n1==n2，否则无环
         while (n1 != n2) {
             if (n2.next == null || n2.next.next == null) {
                 return null;
@@ -54,69 +47,64 @@ public class Code05_FindFirstIntersectNode {
 
     // 如果两个链表都无环，返回第一个相交节点，如果不想交，返回null
     public static Node noLoop(Node head1, Node head2) {
-        if (head1 == null || head2 == null) {
-            return null;
-        }
-        Node cur1 = head1;
-        Node cur2 = head2;
+        Node n1 = head1;
+        Node n2 = head2;
         int n = 0;
-        while (cur1.next != null) {
+        while (n1.next != null) {
             n++;
-            cur1 = cur1.next;
+            n1 = n1.next;
         }
-        while (cur2.next != null) {
+        while (n2.next != null) {
             n--;
-            cur2 = cur2.next;
+            n2 = n2.next;
         }
-        cur1 = n > 0 ? head1 : head2;
-        cur2 = cur1 == head1 ? head2 : head1;
+        n1 = n > 0 ? head1 : head2;
+        n2 = n1 == head1 ? head2 : head1;
         n = Math.abs(n);
-        while (n != 0) {
+        while (n > 0) {
             n--;
-            cur1 = cur1.next;
+            n1 = n1.next;
         }
-        while (cur1 != cur2) {
-            cur1 = cur1.next;
-            cur2 = cur2.next;
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
         }
-        return cur1;
+        return n1;
     }
 
     // 两个有环链表，返回第一个相交节点，如果不相交返回null
     public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
-        Node cur1 = null;
-        Node cur2 = null;
+        Node n1 = head1;
+        Node n2 = head2;
         if (loop1 == loop2) {
-            cur1 = head1;
-            cur2 = head2;
             int n = 0;
-            while (cur1 != loop1) {
+            while (n1.next != loop1) {
                 n++;
-                cur1 = cur1.next;
+                n1 = n1.next;
             }
-            while (cur2 != loop2) {
+            while (n2.next != loop2) {
                 n--;
-                cur2 = cur2.next;
+                n2 = n2.next;
             }
-            cur1 = n > 0 ? head1 : head2;
-            cur2 = cur1 == head1 ? head2 : head1;
+            n1 = n > 0 ? head1 : head2;
+            n2 = n1 == head1 ? head2 : head1;
             n = Math.abs(n);
-            while (n != 0) {
+            while (n > 0){
                 n--;
-                cur1 = cur1.next;
+                n1 = n1.next;
             }
-            while (cur1 != cur2) {
-                cur1 = cur1.next;
-                cur2 = cur2.next;
+            while (n1 != n2) {
+                n1 = n1.next;
+                n2 = n2.next;
             }
-            return cur1;
+            return n1;
         } else {
-            cur1 = loop1.next;
-            while (cur1 != loop1) {
-                if (cur1 == loop2) {
-                    return cur1;
+            Node cur = loop1.next;
+            while (cur != loop1) {
+                if (cur == loop2) {
+                    return cur;
                 }
-                cur1 = cur1.next;
+                cur = cur.next;
             }
             return null;
         }
